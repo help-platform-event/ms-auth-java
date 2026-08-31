9a8e10d0b9ea0bf2e3d8d795b4f5c0d15ccb45a5
Maxime Tavares <t.maxime59@gmail.com>
2026-08-31 14:32:21 +0200

    Add Global Exception Handler

---
+++ src/main/java/com/maxime/help/msauth/web/advice/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.maxime.help.msauth.web.advice;
+
+import com.maxime.help.msauth.application.exception.AuthApplicationException;
+import com.maxime.help.msauth.domain.port.out.GoogleAuthenticationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AuthApplicationException.class)
+    ProblemDetail handleAuthApplicationException(AuthApplicationException ex) {
+        return ProblemDetail.forStatusAndDetail(ex.httpStatus(), ex.getMessage());
+    }
+
+    @ExceptionHandler(GoogleAuthenticationException.class)
+    ProblemDetail handleGoogleAuthenticationException(GoogleAuthenticationException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, "Google authentication failed");
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class})
+    ProblemDetail handleDomainValidation(RuntimeException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+}
